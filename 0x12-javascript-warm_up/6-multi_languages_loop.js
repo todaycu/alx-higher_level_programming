9f0d67238d909c3d35c94125a2de1f2c4706bf8c
Tolera Degefa <100357038+todaycu@users.noreply.github.com>
2022-09-05 19:15:40 +0300

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+for (let i = 0; i < strings.length; i++) {
+  console.log(strings[i]);
 }
