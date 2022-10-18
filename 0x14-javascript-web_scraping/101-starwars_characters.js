b5ae29448c17e2e10e4fffc5c784c3275a3ee359
Tolera Degefa <toleradegefa@outloo.com>
2022-10-18 15:03:40 +0300

    advanced part

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,25 @@
+t request = require('request');
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+
+request.get(url, function (error, response, body) {
+	  if (error) {
+		      console.log(error);
+		    } else {
+			        const role = JSON.parse(body).characters;
+			        print(role, 0);
+			      }
+});
+
+function print (array, i) {
+	  if (i >= array.length) {
+		      return;
+		    }
+	  request.get(array[i], function (error, response, body) {
+		      if (error) {
+			            console.log(error);
+			          } else {
+					        console.log(JSON.parse(body).name);
+					      }
+		      return print(array, i + 1);
+		    });
+}
