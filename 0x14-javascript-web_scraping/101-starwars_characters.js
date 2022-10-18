3fd1552bcce34b3dc4b1416436cb563d4d2294d6
Tolera Degefa <100357038+todaycu@users.noreply.github.com>
2022-10-18 15:07:08 +0300

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,20 @@
-t request = require('request');
-const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
-
-request.get(url, function (error, response, body) {
-	  if (error) {
-		      console.log(error);
-		    } else {
-			        const role = JSON.parse(body).characters;
-			        print(role, 0);
-			      }
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
 });
 
-function print (array, i) {
-	  if (i >= array.length) {
-		      return;
-		    }
-	  request.get(array[i], function (error, response, body) {
-		      if (error) {
-			            console.log(error);
-			          } else {
-					        console.log(JSON.parse(body).name);
-					      }
-		      return print(array, i + 1);
-		    });
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
 }
